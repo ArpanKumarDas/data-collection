53281fc49eb3bc3695581c8239f8679ed284163d
ArpanKumarDas <arpan.kumar.das@gmail.com>
2024-08-31 22:50:32 -0700

    formatting

--- src/components/PersonalInfo.jsx
+++ src/components/PersonalInfo.jsx
@@ -1,138 +1,215 @@
-import React from 'react'
+import React from "react";
 
 const PersonalInfo = ({ nextStep, values, initData, setInitData }) => {
-    const next = e => {
-        e.preventDefault()
+  const next = (e) => {
+    e.preventDefault();
 
-        console.log("body", JSON.stringify(initData));
+    console.log("body", JSON.stringify(initData));
 
-        nextStep()
-    }
+    nextStep();
+  };
 
-    const handleChange = (e) => {
-        setInitData({
-            ...initData,
-            [e.target.name]: e.target.value
-        })
-    }
+  const handleChange = (e) => {
+    setInitData({
+      ...initData,
+      [e.target.name]: e.target.value,
+    });
+  };
 
+  return (
+    <div className="personal_info">
+      <h1>Participant information form</h1>
+      <i>All the fields are required</i>
+      <br />
+      <br />
 
-    return (
-        <div className="personal_info">
-
-            <h1>Participant information form</h1>
-            <i>All the fields are required</i>
-            <br />
-            <br />
-
-            <form
-                onSubmit={next}
+      <form onSubmit={next}>
+        <label>
+          Name:
+          <input
+            type="text"
+            name="name"
+            value={initData.name ?? ""}
+            onChange={handleChange}
+            required
+          />
+        </label>
+        <div className="side-side">
+          <label>
+            Age:
+            <input
+              type="number"
+              name="age"
+              min={15}
+              max={30}
+              value={initData.age ?? ""}
+              onChange={handleChange}
+              required
+            />
+          </label>
+          <label>
+            Gender:
+            <select
+              name="gender"
+              value={initData.gender ?? ""}
+              onChange={handleChange}
+              required
             >
-                <label>
-                    Name:
-                    <input
-                        type="text" name="name" value={initData.name ?? ""} onChange={handleChange} required />
-                </label>
-                <div className='side-side'>
-                    <label>
-                        Age:
-                        <input type="number" name="age" min={15} max={30} value={initData.age ?? ""} onChange={handleChange} required />
-                    </label>
-                    <label>
-                        Gender:
-                        <select name="gender" value={initData.gender ?? ""} onChange={handleChange} required>
-                            <option value="" disabled hidden>--Select--</option>
-                            <option value="male">Male</option>
-                            <option value="female">Female</option>
-                            <option value="non-binary">Non Binary</option>
-                            <option value="other">Other</option>
-                            <option value="pnos">Prefer not to say</option>
-                        </select>
-                    </label>
-                </div>
-                <div className="side-side">
-                    <label>
-                        Degree being pursued:
-                        <input type="text" name="degree" value={initData.degree ?? ""} onChange={handleChange} required />
-                    </label>
-                    <label>
-                        Subject of Study:
-                        <input type="text" name="subject" value={initData.subject ?? ""} onChange={handleChange} required />
-                    </label>
-                </div>
-                <div className="side-side">
-                    <label>
-                        Board of Education at school:
-                        <input type="text" name="board" value={initData.board ?? ""} onChange={handleChange} required />
-                    </label>
-                    <label>
-                        Medium of instruction at school:
-                        <input type="text" name="school_inst" value={initData.school_inst ?? ""} onChange={handleChange} required />
-                    </label>
-                </div>
-                <div className="side-side">
-                    <label>
-                        First language:
-                        <input type="text" name="first_lang" value={initData.first_lang ?? ""} onChange={handleChange} required />
-                    </label>
-                    <label>
-                        Other languages known:
-                        <input type="text" name="other_lang" value={initData.other_lang ?? ""} onChange={handleChange} required />
-                    </label>
-                </div>
-                <label>
-                    State of domicile:
-                    <input type="text" name="state" value={initData.state ?? ""} onChange={handleChange} required />
-                </label>
-                <label>
-                    Parents level of education:
-                    <input type="text" name="parent_edu" value={initData.parent_edu ?? ""} onChange={handleChange} required />
-                </label>
-                <label>
-                    Annual family income:
-                    <select name="income" value={initData.income ?? ""} onChange={handleChange} required>
-                        <option value="" disabled hidden >--Select--</option>
-                        <option value="low">Less than 5 LPA</option>
-                        <option value="medium">Between 5 LPA and 10 LPA</option>
-                        <option value="high">Above 10 LPA</option>
-                    </select>
-                </label>
-                <label>
-                    Phone number:
-                    <input type="tel" name="phno" value={initData.phno ?? ""} onChange={handleChange}
-                        pattern='[0-9]{10}'
-                        title="Please enter 10 digits"
-                        required
-                    />
-                </label>
-                <label>
-                    Email ID:
-                    <input type="email"
-                        name="mail"
-                        value={initData.mail ?? ""}
-                        onChange={handleChange}
-                        placeholder="username@email.com"
-                        pattern="[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,}$"
-                        title="Please match the format: username@email.com (make sure all characters are in lowercase)"
-                        required
-                    />
-                </label>
+              <option value="" disabled hidden>
+                --Select--
+              </option>
+              <option value="male">Male</option>
+              <option value="female">Female</option>
+              <option value="non-binary">Non Binary</option>
+              <option value="other">Other</option>
+              <option value="pnos">Prefer not to say</option>
+            </select>
+          </label>
+        </div>
+        <div className="side-side">
+          <label>
+            Degree being pursued:
+            <input
+              type="text"
+              name="degree"
+              value={initData.degree ?? ""}
+              onChange={handleChange}
+              required
+            />
+          </label>
+          <label>
+            Subject of Study:
+            <input
+              type="text"
+              name="subject"
+              value={initData.subject ?? ""}
+              onChange={handleChange}
+              required
+            />
+          </label>
+        </div>
+        <div className="side-side">
+          <label>
+            Board of Education at school:
+            <input
+              type="text"
+              name="board"
+              value={initData.board ?? ""}
+              onChange={handleChange}
+              required
+            />
+          </label>
+          <label>
+            Medium of instruction at school:
+            <input
+              type="text"
+              name="school_inst"
+              value={initData.school_inst ?? ""}
+              onChange={handleChange}
+              required
+            />
+          </label>
+        </div>
+        <div className="side-side">
+          <label>
+            First language:
+            <input
+              type="text"
+              name="first_lang"
+              value={initData.first_lang ?? ""}
+              onChange={handleChange}
+              required
+            />
+          </label>
+          <label>
+            Other languages known:
+            <input
+              type="text"
+              name="other_lang"
+              value={initData.other_lang ?? ""}
+              onChange={handleChange}
+              required
+            />
+          </label>
+        </div>
+        <label>
+          State of domicile:
+          <input
+            type="text"
+            name="state"
+            value={initData.state ?? ""}
+            onChange={handleChange}
+            required
+          />
+        </label>
+        <label>
+          Parents level of education:
+          <input
+            type="text"
+            name="parent_edu"
+            value={initData.parent_edu ?? ""}
+            onChange={handleChange}
+            required
+          />
+        </label>
+        <label>
+          Annual family income:
+          <select
+            name="income"
+            value={initData.income ?? ""}
+            onChange={handleChange}
+            required
+          >
+            <option value="" disabled hidden>
+              --Select--
+            </option>
+            <option value="low">Less than 5 LPA</option>
+            <option value="medium">Between 5 LPA and 10 LPA</option>
+            <option value="high">Above 10 LPA</option>
+          </select>
+        </label>
+        <label>
+          Phone number:
+          <input
+            type="tel"
+            name="phno"
+            value={initData.phno ?? ""}
+            onChange={handleChange}
+            pattern="[0-9]{10}"
+            title="Please enter 10 digits"
+            required
+          />
+        </label>
+        <label>
+          Email ID:
+          <input
+            type="email"
+            name="mail"
+            value={initData.mail ?? ""}
+            onChange={handleChange}
+            placeholder="username@email.com"
+            pattern="[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,}$"
+            title="Please match the format: username@email.com (make sure all characters are in lowercase)"
+            required
+          />
+        </label>
 
-                {/* make this hidden: phno */}
-                {/* <label>
+        {/* make this hidden: phno */}
+        {/* <label>
                     Participant ID:
                     <input type="text" name="partID" required/>
                 </label> */}
 
-                <button
-                    type='submit'
-                // onClick={next}
-                > Proceed to Questionnaire 1</button>
-            </form>
+        <button
+          type="submit"
+          // onClick={next}
+        >
+          {" "}
+          Proceed to Questionnaire 1
+        </button>
+      </form>
+    </div>
+  );
+};
 
-        </div>
-
-    )
-}
-
-export default PersonalInfo
+export default PersonalInfo;
--- src/components/Sliders.jsx
+++ src/components/Sliders.jsx
@@ -1,83 +1,107 @@
-import React, { memo, useState } from 'react'
-import Slider from '@mui/material/Slider'
-import { marks1, marks2 } from '../data/questionnaire'
-import ErrorIcon from '@mui/icons-material/Error';
-import Tooltip from '@mui/material/Tooltip';
+import React, { memo, useState } from "react";
+import Slider from "@mui/material/Slider";
+import { marks1, marks2 } from "../data/questionnaire";
+import ErrorIcon from "@mui/icons-material/Error";
+import Tooltip from "@mui/material/Tooltip";
 
 const Sliders = ({ qID, ques, state, handleChange, error, overOneSubmit }) => {
-    const minDistance = 1
+  const minDistance = 1;
 
-    const checkValues = (oldValues, newValues, sliderId) => {
-        if (sliderId === 0) {
-            oldValues[1] = [Math.min(newValues[0], oldValues[1][0]), Math.max(newValues[1], oldValues[1][1])]
-        }
-
-        if (sliderId === 1) {
-            oldValues[1] = [Math.min(newValues[0], oldValues[0][0]), Math.max(newValues[1], oldValues[0][1])]
-        }
-
-        return oldValues
+  const checkValues = (oldValues, newValues, sliderId) => {
+    if (sliderId === 0) {
+      oldValues[1] = [
+        Math.min(newValues[0], oldValues[1][0]),
+        Math.max(newValues[1], oldValues[1][1]),
+      ];
     }
 
-    const srija = (event, newValue, sliderId, activeThumb) => {
-        handleChange(oldValue => {
-            const oldVal = [...oldValue]
-
-            console.log(oldVal[sliderId][1], minDistance, oldVal[sliderId][1] - minDistance)
-            if (activeThumb === 0) {
-                oldVal[sliderId] = [Math.min(newValue[0], oldVal[sliderId][1] - minDistance), newValue[1]]
-            } else {
-                oldVal[sliderId] = [newValue[0], Math.max(newValue[1], oldVal[sliderId][0] + minDistance)]
-            }
-
-            return checkValues(oldVal, newValue, sliderId)
-        })
+    if (sliderId === 1) {
+      oldValues[1] = [
+        Math.min(newValues[0], oldValues[0][0]),
+        Math.max(newValues[1], oldValues[0][1]),
+      ];
     }
 
-    const [tooltipOpen, setTooltipOpen] = useState(false);
+    return oldValues;
+  };
 
-    const handleTooltipToggle = () => {
-        setTooltipOpen(!tooltipOpen);
-    };
+  const srija = (event, newValue, sliderId, activeThumb) => {
+    handleChange((oldValue) => {
+      const oldVal = [...oldValue];
 
-    return (<label htmlFor="">
-        <div className={'question-row ' + (overOneSubmit ? error : '')}>
-            <div id={qID} className={"question"}>
-                {qID + ". " + ques}
-            </div>
-            {overOneSubmit && error &&
-                <Tooltip title="Kindly provide a response." arrow interactive open={tooltipOpen} onClick={handleTooltipToggle}>
-                    <ErrorIcon />
-                </Tooltip>
-            }
+      //   console.log(
+      //     oldVal[sliderId][1],
+      //     minDistance,
+      //     oldVal[sliderId][1] - minDistance
+      //   );
+      if (activeThumb === 0) {
+        oldVal[sliderId] = [
+          Math.min(newValue[0], oldVal[sliderId][1] - minDistance),
+          newValue[1],
+        ];
+      } else {
+        oldVal[sliderId] = [
+          newValue[0],
+          Math.max(newValue[1], oldVal[sliderId][0] + minDistance),
+        ];
+      }
+
+      return checkValues(oldVal, newValue, sliderId);
+    });
+  };
+
+  const [tooltipOpen, setTooltipOpen] = useState(false);
+
+  const handleTooltipToggle = () => {
+    setTooltipOpen(!tooltipOpen);
+  };
+
+  return (
+    <label htmlFor="">
+      <div className={"question-row " + (overOneSubmit ? error : "")}>
+        <div id={qID} className={"question"}>
+          {qID + ". " + ques}
         </div>
-        <div className='slider-top'>
-            <span className='slider-lables'>False</span>
-            <span className='slider-lables'>True</span>
-        </div>
-        <Slider
-            getAriaLabel={() => 'Range'}
-            value={state[0]}
-            onChange={(event, newValue, activeThumb) =>
-                srija(event, newValue, 0, activeThumb)
-            }
-            valueLabelDisplay="auto"
-            marks={marks1}
-            disableSwap
-        />
-        <Slider
-            getAriaLabel={() => 'Range'}
-            value={state[1]}
-            onChange={(event, newValue, activeThumb) =>
-                srija(event, newValue, 1, activeThumb)
-            }
-            valueLabelDisplay="auto"
-            marks={marks2}
-            disableSwap
-        />
+        {overOneSubmit && error && (
+          <Tooltip
+            title="Kindly provide a response."
+            arrow
+            interactive
+            open={tooltipOpen}
+            onClick={handleTooltipToggle}
+          >
+            <ErrorIcon />
+          </Tooltip>
+        )}
+      </div>
+      <div className="slider-top">
+        <span className="slider-lables">False</span>
+        <span className="slider-lables">True</span>
+      </div>
+      <Slider
+        getAriaLabel={() => "Range"}
+        value={state[0]}
+        onChange={(event, newValue, activeThumb) =>
+          srija(event, newValue, 0, activeThumb)
+        }
+        valueLabelDisplay="auto"
+        marks={marks1}
+        disableSwap
+      />
+      <Slider
+        getAriaLabel={() => "Range"}
+        value={state[1]}
+        onChange={(event, newValue, activeThumb) =>
+          srija(event, newValue, 1, activeThumb)
+        }
+        valueLabelDisplay="auto"
+        marks={marks2}
+        disableSwap
+      />
 
-        <div className="white-space"></div>
-    </label>)
-}
+      <div className="white-space"></div>
+    </label>
+  );
+};
 
-export default memo(Sliders)
+export default memo(Sliders);
